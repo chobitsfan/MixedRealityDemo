4724f89f48e937f9a03359656de307b0e9e511da
chobits <chobits@itri.org.tw>
2020-10-27 11:28:24 +0800

    ring auto arrange

---
+++ Assets/Rings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rings : MonoBehaviour
+{
+    public GameObject Player;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position = Player.transform.position - Player.transform.right * 6;
+            transform.forward = Player.transform.forward;
+        }
+    }
+}
