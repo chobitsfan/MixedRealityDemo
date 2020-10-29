23a1d91044ea1ae56b34333865e940cca6b018b0
chobitsfan <chobits@itri.org.tw>
2020-10-29 14:15:15 +0800

    add ring hole

---
+++ Assets/Hole.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    public GameObject GoodText;
+    float ts = 0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GoodText.SetActive(true);
+        ts = 1f;
+    }
+
+    private void Update()
+    {
+        if (ts > 0)
+        {
+            ts -= Time.deltaTime;
+            if (ts <= 0)
+            {
+                GoodText.SetActive(false);
+            }
+        }
+    }
+}
