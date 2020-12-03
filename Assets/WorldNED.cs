79937bb856f7fa32f3e2894bf66fc1745a17b9f5
chobits <chobits@itri.org.tw>
2020-12-03 14:38:55 +0800

    r/w worldnorth to reg

---
+++ Assets/WorldNED.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldNED : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("WorldNorth"))
+        {
+            //Debug.Log(PlayerPrefs.GetInt("WorldNorth"));
+            transform.localEulerAngles = new Vector3(0, PlayerPrefs.GetInt("WorldNorth"), 0);
+        }
+        //Debug.Log(transform.localEulerAngles);
+    }
+    private void OnDestroy()
+    {
+        PlayerPrefs.SetInt("WorldNorth", (int)transform.localEulerAngles.y);
+    }
+}
